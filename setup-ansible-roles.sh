96e6679c9040e84dc977cb49d0a179ae98bee740
adm_rechenbergr <robert.werlich@verivox.com>
2018-07-23 11:48:35 +0200

    Initial Commit

---
+++ setup-ansible-roles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone ssh://git@bitbucket.verivox.ads:7999/ar/lin_createlvmmount.git roles/lin_createlvmmount
+git clone ssh://git@bitbucket.verivox.ads:7999/ar/lin_vsphere_domain_join.git roles/lin_vsphere_domain_join
+git clone ssh://git@bitbucket.verivox.ads:7999/ar/readme-generator.git roles/readme-generator
+git clone ssh://git@bitbucket.verivox.ads:7999/ar/vsphere-linux.git roles/vsphere-linux
